7a9f5dc6cfbbacfea32345ebc621bdff9f4ea379
bala-amypo <bala@amypo.in>
2025-12-21 04:26:46 +0000

    User commit

--- src/main/java/com/example/demo/security/CustomUserDetailsService.java
+++ src/main/java/com/example/demo/security/CustomUserDetailsService.java
@@ -4,10 +4,13 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final Map<String, StoredUser> usersByEmail = new HashMap<>();
