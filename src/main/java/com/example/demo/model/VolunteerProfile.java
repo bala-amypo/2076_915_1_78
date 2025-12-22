ec120a4f9ad33df32a17e410865adbdb54f4f973
bala-amypo <bala@amypo.in>
2025-12-22 08:41:49 +0000

    User commit

--- src/main/java/com/example/demo/model/VolunteerProfile.java
+++ src/main/java/com/example/demo/model/VolunteerProfile.java
@@ -12,7 +12,6 @@ public class VolunteerProfile {
 
     private String volunteerId;
     private String fullName;
-    @Email
     private String email;
     private String phone;
     private String availabilityStatus;
