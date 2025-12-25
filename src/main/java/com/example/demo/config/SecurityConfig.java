4f7182ecd4149638f45463a34c635e4acbeecf2c
bala-amypo <bala@amypo.in>
2025-12-25 06:24:11 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,14 +1,17 @@
 package com.example.demo.config;
 
+import com.example.demo.security.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    public JwtTokenProvider jwtTokenProvider() {
+        return new JwtTokenProvider(
+                "VerySecretKeyForJwtDemoApplication123456",
+                3600000L
+        );
     }
 }
