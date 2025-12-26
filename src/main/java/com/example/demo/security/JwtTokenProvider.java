4848d28c00e06dc4d442fbd8dd45d6029c872bfc
bala-amypo <bala@amypo.in>
2025-12-26 05:35:04 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtTokenProvider.java
+++ src/main/java/com/example/demo/security/JwtTokenProvider.java
@@ -8,7 +8,9 @@ import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.stereotype.Component;
 
+@Component
 public class JwtTokenProvider {
 
     private final SecretKey key;
